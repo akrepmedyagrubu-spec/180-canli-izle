b1c4b8ccc22aa6d58385bbd95439fa284b9960ee
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-07-30 20:12:45 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 mkdir -p hls
 
-KV_INDEX=1
-
 python3 -m http.server 10000 &
+
+KV_INDEX=1
 
 while true
 do
@@ -21,23 +21,23 @@ do
   -i reklam_logo.png \
   -filter_complex "
 
-  [0:v][6:v]scale2ref[logo0][v0];[v0][logo0]overlay=0:0[out0];
-  [1:v][7:v]scale2ref[logo1][v1];[v1][logo1]overlay=0:0[out1];
-  [2:v][6:v]scale2ref[logo2][v2];[v2][logo2]overlay=0:0[out2];
-  [3:v][6:v]scale2ref[logo3][v3];[v3][logo3]overlay=0:0[out3];
-  [4:v][6:v]scale2ref[logo4][v4];[v4][logo4]overlay=0:0[out4];
-  [5:v][6:v]scale2ref[logo5][v5];[v5][logo5]overlay=0:0[out5];
+  [0:v][6:v]overlay=0:0[v0];
+  [1:v][7:v]overlay=0:0[v1];
+  [2:v][6:v]overlay=0:0[v2];
+  [3:v][6:v]overlay=0:0[v3];
+  [4:v][6:v]overlay=0:0[v4];
+  [5:v][6:v]overlay=0:0[v5];
 
-  [out0][0:a]
-  [out1][1:a]
-  [out2][2:a]
-  [out3][3:a]
-  [out4][4:a]
-  [out5][5:a]
+  [v0][0:a]
+  [v1][1:a]
+  [v2][2:a]
+  [v3][3:a]
+  [v4][4:a]
+  [v5][5:a]
   concat=n=6:v=1:a=1[v][a]
   " \
   -map "[v]" -map "[a]" \
-  -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" \
+  -vf "scale=1280:720,format=yuv420p" \
   -c:v libx264 -preset veryfast -crf 23 \
   -c:a aac -b:a 128k \
   -f hls \
