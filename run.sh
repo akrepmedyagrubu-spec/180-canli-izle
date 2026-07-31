b82e66f4cfaf071b8643a288718bcd8bf3e3df18
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-07-31 22:53:30 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,30 @@
-  #!/bin/bash
+#!/bin/bash
 
 mkdir -p hls
 
 python3 -m http.server 10000 &
 
-KV_INDEX=1
+echo "Yayın başladı..."
 
 while true
 do
-  for FILE in rj1.mp4 reklam.mp4 rj2.mp4 jenerik.mp4 akilli.mp4 kv${KV_INDEX}.mp4
+  while read FILE
   do
     echo "Oynatılıyor: $FILE"
 
-    if [[ "$FILE" == "reklam.mp4" ]]; then
+    BASENAME=$(basename "$FILE")
+
+    # LOGO SEÇİMİ
+    if [[ "$BASENAME" == "reklam.mp4" ]]; then
       LOGO="reklam_logo.png"
     else
       LOGO="yayin_logo.png"
     fi
 
+    echo "Logo: $LOGO"
+
     ffmpeg -re -i "$FILE" -i "$LOGO" \
-    -filter_complex "[0:v]scale=1280:720,fps=25[v];[1:v]scale=1280:720[l];[v][l]overlay=0:0" \
+    -filter_complex "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,fps=25[v];[1:v]scale=1280:720[l];[v][l]overlay=0:0" \
     -map 0:a? \
     -c:v libx264 -preset ultrafast -crf 28 \
     -c:a aac -b:a 96k \
@@ -29,12 +34,6 @@ do
     -hls_flags delete_segments+append_list \
     hls/stream.m3u8
 
-  done
-
-  ((KV_INDEX++))
-
-  if [ ! -f "kv${KV_INDEX}.mp4" ]; then
-    KV_INDEX=1
-  fi
+  done < playlist.txt
 
 done
