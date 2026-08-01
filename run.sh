5bfc7345f68f7f327687146702c1caef46ef0faa
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-08-01 23:29:40 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,10 @@ mkdir -p hls
 
 python3 -m http.server 10000 &
 
-echo "PLAYLIST YAYIN BASLADI"
+echo "DOSYALAR:"
+ls -lh
+
+echo "YAYIN BASLADI"
 
 while true
 do
@@ -14,11 +17,15 @@ do
 while read VIDEO
 do
 
-echo "Oynatiliyor: $VIDEO"
+echo "VIDEO: $VIDEO"
+
+if [ ! -f "$VIDEO" ]; then
+  echo "DOSYA YOK: $VIDEO"
+  continue
+fi
 
 ffmpeg -re -i "$VIDEO" -i yayin_logo.png \
--filter_complex "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,\
-pad=1280:720:(ow-iw)/2:(oh-ih)/2[v];[1:v]scale=1280:720[l];[v][l]overlay=0:0" \
+-filter_complex "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2[v];[1:v]scale=1280:720[l];[v][l]overlay=0:0" \
 -map 0:a? \
 -c:v libx264 -preset ultrafast -crf 28 \
 -c:a aac -b:a 96k \
