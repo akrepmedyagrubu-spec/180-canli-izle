3e94bc2637534382198c907bd1ba3ae25d886b2f
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-08-01 23:10:59 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,21 +6,28 @@ mkdir -p hls
 
 python3 -m http.server 10000 &
 
-echo "OTOMATİK YAYIN BAŞLADI"
+echo "PLAYLIST YAYIN BASLADI"
 
 while true
 do
 
-ffmpeg -re -stream_loop -1 -fflags +genpts -f concat -safe 0 -i liste.txt -i yayin_logo.png \
--filter_complex "[0:v]scale=1280:720,fps=25,format=yuv420p[v];[1:v]scale=1280:720[l];[v][l]overlay=0:0" \
+while read VIDEO
+do
+
+echo "Oynatiliyor: $VIDEO"
+
+ffmpeg -re -i "$VIDEO" -i yayin_logo.png \
+-filter_complex "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,\
+pad=1280:720:(ow-iw)/2:(oh-ih)/2[v];[1:v]scale=1280:720[l];[v][l]overlay=0:0" \
 -map 0:a? \
--c:v libx264 -preset ultrafast -crf 28 -pix_fmt yuv420p \
--c:a aac -ar 44100 -ac 2 -b:a 96k \
--af "aresample=async=1" \
+-c:v libx264 -preset ultrafast -crf 28 \
+-c:a aac -b:a 96k \
 -f hls \
 -hls_time 4 \
 -hls_list_size 6 \
--hls_flags delete_segments+append_list+split_by_time \
+-hls_flags delete_segments \
 hls/stream.m3u8
+
+done < liste.txt
 
 done
