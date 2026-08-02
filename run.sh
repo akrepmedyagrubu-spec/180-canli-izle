4160e9bd51a4fd3205aeb2870fc5683ca45852e7
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-08-02 14:47:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,37 +4,34 @@ cd /app || exit
 
 mkdir -p hls
 
+# web server
 python3 -m http.server 10000 &
-
-echo "DOSYALAR:"
-ls -lh
 
 echo "YAYIN BASLADI"
 
 while true
 do
 
-while read VIDEO
-do
-
-echo "VIDEO: $VIDEO"
-
-if [ ! -f "$VIDEO" ]; then
-  echo "DOSYA YOK: $VIDEO"
-  continue
-fi
-
-ffmpeg -re -i "$VIDEO" -i yayin_logo.png \
--filter_complex "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2[v];[1:v]scale=1280:720[l];[v][l]overlay=0:0" \
--map 0:a? \
+ffmpeg -re \
+-i rj1.mp4 \
+-i reklam.mp4 \
+-i rj2.mp4 \
+-i jenerik.mp4 \
+-i akilli.mp4 \
+-i panasonicsunar.mp4 \
+-i kv1.mp4 \
+-i panasonicsundu.mp4 \
+-filter_complex "[0:v][0:a][1:v][1:a][2:v][2:a][3:v][3:a][4:v][4:a][5:v][5:a][6:v][6:a][7:v][7:a]concat=n=8:v=1:a=1[outv][outa]" \
+-map "[outv]" -map "[outa]" \
 -c:v libx264 -preset ultrafast -crf 28 \
--c:a aac -b:a 96k \
+-vf "scale=1280:720,fps=25" \
+-c:a aac -b:a 128k -ar 44100 -ac 2 \
 -f hls \
 -hls_time 4 \
 -hls_list_size 6 \
--hls_flags delete_segments \
+-hls_flags delete_segments+append_list \
 hls/stream.m3u8
 
-done < liste.txt
+echo "FFMPEG yeniden baslatiliyor..."
 
 done
