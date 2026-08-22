b6624f77657eec7311ee28f849eb657e8de07bbc
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-08-22 23:06:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,14 @@
-const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
+import TelegramBot from 'node-telegram-bot-api';
 
 const TOKEN = "8854368089:AAGuSqO3TaPNTo1Ya6ojSf4YlNGQ71oiXM0";
-
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-let list = [];
+console.log("BOT AKTİF");
 
-if (fs.existsSync("playlist.json")) {
-  list = JSON.parse(fs.readFileSync("playlist.json"));
-}
+bot.on('channel_post', (msg) => {
+  if (!msg.video) return;
 
-bot.on('message', (msg) => {
-  if (msg.video) {
-    list.push(msg.video.file_id);
-    fs.writeFileSync("playlist.json", JSON.stringify(list, null, 2));
-    console.log("video eklendi");
-  }
+  const fileId = msg.video.file_id;
+
+  console.log("FILE_ID:", fileId);
 });
