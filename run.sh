d6ca17b20bc320fe2517a201d0c14bd351c30c59
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-08-22 23:27:38 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,21 +4,20 @@ mkdir -p hls
 
 python3 -m http.server 10000 &
 
-while true
+while IFS=":" read TYPE FILE_ID
 do
-  while read FILE
-  do
-    if [[ "$FILE" == *"reklam"* ]]; then
-      LOGO="reklam_logo.png"
-    else
-      LOGO="yayin_logo.png"
-    fi
+  FILE="videos/${FILE_ID}.mp4"
 
-    ffmpeg -re -i "$FILE" -i "$LOGO" \
-    -filter_complex "[0:v]scale=960:540,pad=960:540:(ow-iw)/2:(oh-ih)/2[v0];[1:v][v0]overlay=0:0" \
-    -c:v libx264 -preset ultrafast \
-    -f hls -hls_time 4 -hls_list_size 6 \
-    hls/stream.m3u8
+  if [[ "$TYPE" == "reklam" ]]; then
+    LOGO="reklam_logo.png"
+  else
+    LOGO="yayin_logo.png"
+  fi
 
-  done < playlist.txt
-done
+  ffmpeg -re -i "$FILE" -i "$LOGO" \
+  -filter_complex "[0:v]scale=960:540,pad=960:540[v0];[1:v][v0]overlay=0:0" \
+  -c:v libx264 -preset ultrafast \
+  -f hls -hls_time 4 -hls_list_size 6 \
+  hls/stream.m3u8
+
+done < playlist.txt
