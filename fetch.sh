c9bd1ed02b67d46b983b4c5d151d94644d0167ea
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-08-22 23:27:16 +0300

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 TOKEN="8854368089:AAGuSqO3TaPNTo1Ya6ojSf4YlNGQ71oiXM0"
-
 mkdir -p videos
 
-cat db.json | jq -c '.[]' | while read item
+while IFS=":" read TYPE FILE_ID
 do
-  FILE_ID=$(echo $item | jq -r '.file_id')
-  CAPTION=$(echo $item | jq -r '.caption')
-
   FILE_PATH=$(curl -s "https://api.telegram.org/bot$TOKEN/getFile?file_id=$FILE_ID" \
   | grep -o '"file_path":"[^"]*' | cut -d'"' -f4)
 
@@ -16,6 +12,8 @@ do
 
   NAME="videos/${FILE_ID}.mp4"
 
-  echo "indiriliyor: $NAME"
+  echo "indiriliyor: $TYPE"
+
   wget -q -O "$NAME" "$URL"
-done
+
+done < playlist.txt
